7d07c1373cad0091e73dc3b7d41e69dfc25c7a9b
Graceas <grac.ga@gmail.com>
2019-10-22 20:33:50 +0700

    add request builder rule

---
+++ src/ScraperEngine/rules/RequestsBuilderRule.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gorelov
+ * Date: 2019-10-19
+ * Time: 22:47
+ */
+
+namespace ScraperEngine\Rules;
+
+/**
+ * Class RequestsBuilderRule
+ * @package ScraperEngine\Rules
+ */
+class RequestsBuilderRule extends BaseRule
+{
+    /**
+     * settings:
+     * parameters - array of parameters for request
+     * @param array $storage
+     * @return mixed
+     */
+    public function execute($storage)
+    {
+        $requests = array();
+        $parameters = (isset($this->settings['parameters'])) ? $this->settings['parameters'] : $storage[$this->required[0]];
+
+        foreach ($parameters as $parameter) {
+            $request = call_user_func_array($this->settings['create_request_function'], array($parameter));
+            if (is_array($request)) {
+                $requests = array_merge($requests, $request);
+            } else {
+                $requests[] = $request;
+            }
+        }
+
+        $requests = array_filter($requests);
+
+        return $requests;
+    }
+}
